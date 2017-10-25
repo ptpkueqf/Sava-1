2bfd12d56bc496c96546be100a477db02dbdf03f
songwei3 <songwei3@illinois.edu>
2017-10-25 00:37:08 -0500

    Update FileSendThread.java

--- src/main/java/sdfs/FileSendThread.java
+++ src/main/java/sdfs/FileSendThread.java
@@ -2,41 +2,79 @@ package sdfs;
 
 import org.apache.log4j.Logger;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.io.*;
+import java.net.*;
 
 /*** This class is used to send the file message to other nodes.
  * *
  */
 public class FileSendThread extends Thread{
     public static Logger logger = Logger.getLogger(FileSendThread.class);
+    public static String[] sendMessage;
     LeaderElection leader = new LeaderElection();
     String leaderIp = leader.getLeaderIp();
+    int leaderPort =  leader.getLeaderPort();
 
     public void run(){
+
+        //TODO
+    }
+
+
+    // check whether the current is master
+    public boolean isLeader(){
         try {
             String machineIp = InetAddress.getLocalHost().getHostAddress().toString();
+            if(machineIp == leaderIp ){
+                return true;
+            }
         } catch (UnknownHostException e) {
             logger.error(e);
             e.printStackTrace();
         }
+        return false;
+    }
 
-    }
-    public void putFile(String localfilename, String sdfsfilename){
+    public void sendMessageOp(String[] message){
+        //TODO
 
-    }
-    public void getFile(String localfilename, String sdfsfilename){
+        // if the current machine is leader, then it doesn't need to send message
+        if(isLeader()){
+            FileReceiveThread op = new FileReceiveThread();
+            op.leaderOp(message);
+        }else {
+            // send message to the leader
+            DatagramSocket socket = null;
+            try {
+                socket = new DatagramSocket();
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
 
-    }
-    public void deleteFile(String sdfsfilename){
 
+                objectOutputStream.writeObject(message);
+
+                byte[] buffer = byteArrayOutputStream.toByteArray();
+                int length = buffer.length;
+                DatagramPacket datagramPacket = new DatagramPacket(buffer, length);
+                datagramPacket.setAddress(InetAddress.getByName(leaderIp));
+                datagramPacket.setPort(leaderPort);
+
+                socket.send(datagramPacket);
+
+            } catch (SocketException e) {
+                e.printStackTrace();
+                logger.error(e);
+            } catch (IOException e) {
+                e.printStackTrace();
+                logger.error(e);
+            }
+        }
     }
-    public String[] listMembers(String sdfsfilename){
-        String[] addresses = null;
-        return addresses;
+    public String[] queryListOp(String[] message){
+        //TODO
+        String[] list = null;
+        sendMessageOp(message);
+        return list;
     }
-    public String[] listFiles(String machineId){
-        String[] fileNames = null;
-        return fileNames;
-    }
+
 }
